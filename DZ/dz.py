2911558f7a2b3687dd212ec803443d7e427c75d7
Din <hurshid.17.97@mail.ru>
2022-12-03 13:46:41 +0500

    dz

--- DZ/dz.py
+++ DZ/dz.py
@@ -17,6 +17,12 @@
 #2.Напишите программу для. проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 #  ⋀ - and ⋁ - or ¬ - not
+print('Утверждение истино при: ')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if (not(x or y or z)) == (not x and y and not z):
+                print (f'x = {bool(x)},\t y = {bool(y)},\t z ={bool(z)}')
         
 
 
@@ -29,6 +35,21 @@
 
 x = int(input('Введите число x≠0:'))
 y = int(input('Введите число y≠0:'))
+def inputKoord (x):
+    x == [0] * x
+    for i in range (x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = float(input(f"Введите {i+1} координату: "))
+                a[i] = number
+                is_OK = True
+                if a[i] == 0:
+                    is_OK = False
+                    print("Координата не должно быть равна 0 ")
+            except ValueError:
+                print("Error! Введите пожалуйста числа!")
+    return 
 def Coordinates(xy):
     text = 4
     if xy[0] > 0 and xy[1] > 0:
@@ -45,8 +66,31 @@ def Coordinates(xy):
 
 #4.Напишите программу, которая по заданному номеру четверти, 
 # показывает диапазон возможных координат точек в этой четверти (x и y).
+
+import os
+os.system('cls')
+quarter = int(input('Введите номер четверти: '))
+
+if quarter == 1:
+    print('x > 0, y > 0')
+elif quarter == 2:
+    print('x < 0, y > 0')
+elif quarter == 3:
+    print('x < 0, y < 0')
+elif quarter == 4:
+    print('x > 0, y < 0')
+else:
+    print('На координатной плоскости только 4 четверти')
+
 #5.Напишите программу, которая принимает на вход координаты двух точек и находит
 # расстояние между ними в 2D пространстве.
 #Пример:
 #- A (3,6); B (2,1) -> 5,09
 #- A (7,-5); B (1,-1) -> 7,21
+x_cordinate_A = float(input('Введите координату точки A по оси X: '))
+y_cordinate_A = float(input('Введите координату точки A по оси Y: '))
+x_cordinate_B = float(input('Введите координату точки B по оси X: '))
+y_cordinate_B = float(input('Введите координату точки B по оси Y: '))
+
+distance = (((x_cordinate_B - x_cordinate_A)**2 + (y_cordinate_B - y_cordinate_A)**2)**0,5)
+print(distance)
